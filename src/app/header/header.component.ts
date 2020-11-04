6ef194802836d6d37da60678a381606456a9f02b
= <=>
2020-11-04 15:02:14 +0700

    Update fix sum total

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from './../product.model';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  @Input()
+  products: Product[];
 
-  //khai báo chặt chẽ hơn thì thêm :kiểu giá trị vào trong khai báo biến
-  totalItems: number = 50 ;
-
+  getTotalItems() {
+    let totalItems: number;
+    for (let i = 0; i < this.products.length; i++) {
+      const item = this.products[i];
+      totalItems += item.quantity;
+    }
+    return totalItems;
+  }
 }
--- src/app/promotion/promotion.component.ts
+++ src/app/promotion/promotion.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from './../product.model';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-promotion',
@@ -6,5 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./promotion.component.css'],
 })
 export class PromotionComponent {
-  subTotal: number = 21.97;
+  @Input() products: Product[];
+
+  getTotalSummary() {
+    console.log('this.products', this.products);
+    let subTotal: number;
+    for (let i = 0; i < this.products.length; i++) {
+      // const item = this.products[i];
+      subTotal += this.products[i].price * this.products[i].quantity;
+    }
+    console.log('subTotal', subTotal);
+    return subTotal;
+  }
 }
